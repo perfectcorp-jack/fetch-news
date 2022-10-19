a2866072275fafc0147ea71b87b175d1339fddff
ChiaYuSu <jack879tw@gmail.com>
2022-10-19 20:14:27 +0800

    add 'Scrolling to bottom loads another page of headlines' func

--- src/App.js
+++ src/App.js
@@ -9,39 +9,57 @@ class App extends React.Component {
       isLoaded: false,
       results: null,
       category: '',
+      offset: 0,
     };
     this.handleSelect = this.handleSelect.bind(this);
   }
 
   componentDidMount() {
     window.addEventListener('scroll', () => {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         console.log('you are at the bottom of the page');
-        setTimeout(() => {
-          this.handlefetch(this.state.category);
-        }, 5000);
-        // console.log(this.state.category);
-        // this.handlefetch(this.state.category);
+        this.handlefetch(this.state.category, true);
+        window.scrollTo(0, document.body.scrollHeight / 2);
       }
     })
   }
 
   // 44bbac4daf4709e837068fed365ea4e3 // finish
   // c085361734abb2056c9617f340a42999 // finish
-  // 48b7b01739d784475ec9b17bd665979b
-  // 89a9652ad732f6521e5fc736c0bf102d
+  // 48b7b01739d784475ec9b17bd665979b 
+  // 89a9652ad732f6521e5fc736c0bf102d // finish
 
-  handlefetch(category) {
-    fetch("http://api.mediastack.com/v1/news?access_key=c085361734abb2056c9617f340a42999&categories=" + category + "&countries=us&limit=25", {})
+  handlefetch(category, cf) {
+    const url = 'http://api.mediastack.com/v1/news';
+    const apiKey = '?access_key=' + '48b7b01739d784475ec9b17bd665979b';
+    const categories = `&categories=${category}`;
+    const countries = '&countries=us';
+    const limit = '&limit=25';
+    const offset = `&offset=${this.state.offset}`;
+    const sort = '&sort=published_desc';
+    this.setState({
+      offset: this.state.offset + 25,
+    });
+    fetch(url + apiKey + categories + countries + limit + offset + sort, {})
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
-        this.setState({
-          isLoaded: true,
-          results: data,
-        });
+        // console.log(data);
+        // console.log(this.state.offset);
+        if ((data.data.length > 0) && (this.state.category === category) && (cf)) {
+          this.setState({
+            isLoaded: true,
+            results: this.state.results ? { data: [...this.state.results.data, ...data.data] } : data,
+          });
+          // console.log(this.state.offset)
+        } else {
+          this.setState({
+            isLoaded: true,
+            results: data,
+            offset: 0,
+          });
+        }
       })
       .catch((error) => {
         this.setState({
@@ -52,10 +70,12 @@ class App extends React.Component {
   }
 
   handleSelect(e) {
+    const cf = this.state.category && e.target.value === this.state.category;
+    // console.log(cf);
     this.setState({
       category: e.target.value,
     });
-    this.handlefetch(e.target.value);
+    this.handlefetch(e.target.value, cf);
   }
 
   render() {
@@ -89,7 +109,6 @@ class App extends React.Component {
             </select>
           </div>
           {results.data
-            .sort((a, b) => a.published_at < b.published_at ? 1 : -1) // sort by date descending
             .map((result) => {
               return (
                 <div style={{ width: 1000, margin: 'auto' }}>
@@ -98,7 +117,8 @@ class App extends React.Component {
                       {result.image ? <img src={result.image} style={{ borderRadius: 10, width: '100%' }} alt='news' /> : <img src='https://v4.tocas-ui.com/zh-tw/assets/images/1-1.png' style={{ borderRadius: 10, width: '100%' }} alt='news' />}
                     </div>
                     <div className='text'>
-                      <span className='date'>{result.published_at.slice(0, 10)}</span> <span className='time'>{result.published_at.slice(11, 19)}</span>｜
+                      <span className='date'>{result.published_at.slice(0, 10)}</span> &nbsp;
+                      <span className='time'>{result.published_at.slice(11, 19)}</span>｜
                       <span className='category'>{result.category}</span>｜
                       <span className='author'>{result.author ? result.author : 'No author'}</span>
                       <h2>{result.title}</h2>
